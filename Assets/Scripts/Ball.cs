b22fcb84ae4332b9850f0d17fbce980e22425ef6
Dmytro Kuchura <dmytro.kuchura@amadeus.com>
2020-08-22 00:40:54 +0300

    init

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    public GameObject PointTop;
+    public GameObject PointBottom;
+    public Text Score;
+
+    bool check = true;
+    int score = 1;
+
+    public ParticleSystem Dead;
+
+    void Start()
+    {
+        Dead.Clear();
+        Dead.Stop();
+    }
+
+    void Update()
+    {
+        if (check)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, PointTop.transform.position, 2f * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, PointBottom.transform.position, 2f * Time.deltaTime);
+        }
+
+    }
+
+    void Change()
+    {
+        check = !check;
+    }
+
+    void Crash()
+    {
+        Dead.Clear();
+        Dead.Play();
+    }
+
+    void Increment()
+    {
+        score++;
+        Score.text = score.ToString() + "Pts";
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform transform;
+    
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        transform.rotation *= Quaternion.Euler(0f, 0f, 0.2f);
+        transform.position += new Vector3(1, 0, 0) * Time.deltaTime * 1;
+    }
+}
